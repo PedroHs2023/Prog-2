83cb116d025afa6bbafa93ac68ffd75447ff0491
Pedro Henrique Salvador da Silva <108558563+PedroHs2023@users.noreply.github.com>
2024-09-11 16:34:37 -0300

    Add files via upload

---
+++ Point2.cpp
@@ -0,0 +1,17 @@
+#include "point2d.hpp"
+
+int main()
+{
+    Point2D p1;
+    Point2D p2(10,20);
+
+    Point2D *p3 = new Point2D();
+    Point2D *p4 = new Point2D(30,20);
+
+    //checagem de memoria:
+    //valgrind --leak-check=full ./out
+    delete p3;
+    delete p4;
+
+    return 0;
+}
